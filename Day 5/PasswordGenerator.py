d099f5d419c46d39e88e413c6f9b9900dc568ada
Whisper-Development <alberto.lupi.dev@gmail.com>
2023-10-24 18:05:24 +0200

    Day 5 part 2

---
+++ Day 5/PasswordGenerator.py
@@ -0,0 +1,34 @@
+# Password Generator
+
+# Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input("How many symbols?\n"))
+nr_numbers = int(input("How many numbers?\n"))
+
+# Eazy Level - Order not randomised
+
+password = ""
+
+# Generating letters
+for l in range(nr_letters):
+    password += random.choice(letters)
+
+# Generating symbols
+
+for s in range(nr_symbols):
+    password += random.choice(symbols)
+
+# Generating Numbers
+for n in range(nr_numbers):
+    password += random.choice(numbers)
+
+print("Generated password:", password)
+
+
