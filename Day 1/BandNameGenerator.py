24b4f8d43812ec939388864ec8cae1ea0a93619a
Whisper-Development <alberto.lupi.dev@gmail.com>
2023-10-24 15:53:39 +0200

    Day 1

---
+++ Day 1/BandNameGenerator.py
@@ -0,0 +1,10 @@
+#Greetings
+
+print("Welcome to the Band Name Generator v1.0")
+
+# Inputs
+city = input("Where do you live?\n")
+pet = input("What's your pet's name?\n")
+
+# Band name
+print(f"Your Band should be named: {city}'s {pet}")
